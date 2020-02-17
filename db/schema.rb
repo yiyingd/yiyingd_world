4127d01f413da8376370e07776339174c6164183
Amanda Bao <abao@andrew.cmu.edu>
2020-02-17 12:27:35 -0500

    Revert "Added Article model and relationship w Category, validations and scopes"
    This reverts commit 93d98c6055827a661643371a3e6236b68c88fd30.

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200217170311) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "category_id"
-    t.boolean "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170919031503) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
