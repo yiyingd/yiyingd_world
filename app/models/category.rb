bebaa14fecc06399b0bc7078abc1bce055f8eaf8
yiyingd <49003278+yiyingd@users.noreply.github.com>
2020-02-18 16:10:22 -0500

    Revert "Articles model"

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,7 @@
 class Category < ApplicationRecord
 	has_many :photos
-	has_many :articles
 
 	scope :active, 			 -> { where('active = ?', true) }
-	scope :alphabetical, -> { order('name') }
+  scope :alphabetical, -> { order('name') }
   
 end
